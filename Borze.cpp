827de64bb7e718b7b7fd1c2a5fc70f0b5091c420
hughnhugh <hughnguyen@outlook.com>
2023-11-13 16:59:37 +1100

    solve: add solution for Borze

--- Borze.cpp
+++ Borze.cpp
@@ -4,12 +4,22 @@
 using namespace std;
 
 int main() {
-    string input;
+    string s;
 
-    cin >> input;
+    cin >> s;
 
+    string res = "";
 
+    for (int i=0; i<s.length(); i++) {
+        if (s[i] == '.') res+="0";
+        else if (s[i] == '-') {
+            if (s[i+1] == '.') res+="1";
+            else if (s[i+1] == '-') res+="2";
+            i++;
+        }
+    }
 
+    cout << res << endl;
 
     return 0;
 }
