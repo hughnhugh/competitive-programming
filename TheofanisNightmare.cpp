3f5df85e985d1c508e5a227b4f146b27b3626b53
hughnhugh <hughnguyen@outlook.com>
2023-12-02 02:14:22 +1100

    partial: 1903C

--- TheofanisNightmare.cpp
+++ TheofanisNightmare.cpp
@@ -7,7 +7,7 @@ int main() {
     int t;
     cin >> t;
 
-    int res[t];
+    vector<int> res(t);
 
     for (int i=0; i<t; i++) {
         int n;
@@ -23,18 +23,35 @@ int main() {
         int index = 0;
         int sum = 0;
         int maxValue = 0;
+        int recentSum = 0;
 
         for (int j=n-1; j>=0; j--) {
             int current = nums[j];
+            bool addBefore = false;
+
+            if (sum < 0 && sum + current > 0) {
+                addBefore = true;
+            }
             sum += current;
 
-            if (sum >= 0) {
+            if (addBefore) {
+                if (subs.size() == 0) {
+                    subs.push_back(0);
+                    index++;
+                }
+                maxValue += current;
+                subs[index-1] += current;
+            }
+            else if (sum >= 0) {
                 maxValue += sum;
                 subs.push_back(current);
                 index++;
             } 
             else {
-                if (subs.size() == 0) subs.push_back(0);
+                if (subs.size() == 0) {
+                    subs.push_back(0);
+                    index++;
+                }
                 maxValue += current;
                 subs[index-1] += current;
             }
