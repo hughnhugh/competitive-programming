276f7a7d9aab76d5705dfd5f07ce7281446af019
hughnhugh <hughnguyen@outlook.com>
2023-12-04 03:59:35 +1100

    fix: improve variable naming

--- Boilerplate.cpp
+++ Boilerplate.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int t;
-    cin>>t;
+    int test;
+    cin >> test;
     
-    while(t--){
+    while(test--){
     }
 }
