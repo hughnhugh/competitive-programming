8bac8cbca494642e038e6be02bf0d42ed914e0ae
hughnhugh <hughnguyen@outlook.com>
2023-12-11 14:14:26 +1100

    contest: ABC332 - Unsolved F

---
+++ ABC332/RandomUpdateQuery.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void incrementSubarray(vector<int>& arr, int start, int end) {
+    for (int i = start; i <= end; ++i) {
+        arr[i]++;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n, m;
+    cin >> n >> m;
+
+    vector<long long> arr(n);
+    vector<long long> prefix(n+1);
+
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+        prefix[i+1] = prefix[i] + arr[i];
+    }
+
+    vector<long long> arrCounts(n);
+
+    for (int i=0; i<m; i++) {
+        int l, r, x;
+        cin >> l >> r >> x;
+
+        int sum = sumAll(l, r, arr)/(r-l+1);
+
+        for (int j=l; j <= r; j++) {
+            arr[j] = ((r-l+1) * arr[j] + sum)/(r-l+1);
+        }
+    }
+
+}
