8d131e88eb49a4aca9a82fd6db3213b97d802fb1
hughnhugh <hughnguyen@outlook.com>
2023-12-17 04:22:23 +1100

    contest: 1905A

---
+++ Round 915 - Div. 2/ConstructiveProblems.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int test;
+    cin >> test;
+    
+    while (test--) {
+        int n, m;
+        cin >> n >> m;
+        cout << max(n, m) << "\n";
+    }
+}
